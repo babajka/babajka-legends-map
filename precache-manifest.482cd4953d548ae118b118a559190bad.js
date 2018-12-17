6c42742afed4e14e4b1a01f63c3039fbe0556b5c
Ivan Pazhitnykh <ivan.pazhitnykh@gmail.com>
2018-12-17 09:51:27 +0300

    Updates

---
+++ precache-manifest.482cd4953d548ae118b118a559190bad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a704dde7fb442f4bb4e",
+    "url": "/babajka-legends-map/static/js/runtime~main.2a704dde.js"
+  },
+  {
+    "revision": "efc86a4b98748f62262c",
+    "url": "/babajka-legends-map/static/js/main.efc86a4b.chunk.js"
+  },
+  {
+    "revision": "c6a83c96ee042a4922b7",
+    "url": "/babajka-legends-map/static/js/1.c6a83c96.chunk.js"
+  },
+  {
+    "revision": "efc86a4b98748f62262c",
+    "url": "/babajka-legends-map/static/css/main.158449b6.chunk.css"
+  },
+  {
+    "revision": "c6a83c96ee042a4922b7",
+    "url": "/babajka-legends-map/static/css/1.47eebd83.chunk.css"
+  },
+  {
+    "revision": "73a188abc30d2ca47657f7719934d47c",
+    "url": "/babajka-legends-map/index.html"
+  }
+];
