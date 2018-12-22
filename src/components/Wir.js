64d058c376a5cfdba32cbcb738806a40fb822bf3
Ivan Pazhitnykh <ivan.pazhitnykh@gmail.com>
2018-12-22 22:14:02 +0200

    replace fa with svg, change logo image

--- src/components/Wir.js
+++ src/components/Wir.js
@@ -4,7 +4,7 @@ import { zIndexes, zIndexElements } from '../constants';
 
 const Wir = () => (
   <div className="wir" style={{ zIndex: zIndexes[zIndexElements.LABELS] }}>
-    <img className="wir__logo" src="./images/logo.png" alt="Wir.by" />
+    <img className="wir__logo" src="./favicon.png" alt="Wir.by" />
     <div>
       зроблена камандай
       <a
