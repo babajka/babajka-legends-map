cf93b4ce7fd8be57ab700558863273eba7d3a726
Ivan Pazhitnykh <ivan.pazhitnykh@gmail.com>
2018-12-22 22:59:02 +0200

    prettier config, lint-staged, pointer cursor fixes

---
+++ stylelint.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: ['stylelint-config-idiomatic-order', './node_modules/prettier-stylelint/config.js'],
+  rules: {
+    indentation: 4,
+    'string-quotes': 'single',
+  },
+};
